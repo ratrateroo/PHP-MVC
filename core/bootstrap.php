d921ddcec05c145912eea2cfd6a58e6eaaec8dd1
Mark Leo <ratrateroo@gmail.com>
2019-08-28 02:37:41 +0800

    setup bootstrap file

---
+++ core/bootstrap.php
@@ -0,0 +1,21 @@
+<?php 
+//load configuration and helper functions
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+
+// autoload classes
+
+function __autoload($className) {
+    if(file_exists(ROOT . DS . 'core' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'core' . DS . $className . '.php');
+    } elseif (file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
+    }
+    elseif (file_exists(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php');
+    }
+}
+
+// route the request
+
+Router::route($url);
