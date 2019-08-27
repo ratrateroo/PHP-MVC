8765b71807949d5144f3b8c2cafb3604828df2bd
Mark Leo <ratrateroo@gmail.com>
2019-08-28 03:29:30 +0800

    setup application class

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php 
+define('DEBUG', true);
+define('DEFAULT_CONTROLLER', 'Home'); //default controller if there isn't one defined in the url
--- core/Router.php
+++ core/Router.php
@@ -2,7 +2,30 @@
 
 class Router {
     public static function route($url){
-        dnd($url);
+        
+        //controller
+
+        $controller = (isset($url[0]) && $url[0] != '') ? ucfirst($url[0]): DEFAULT_CONTROLLER;
+        $controller_name = $controller;
+        array_shift($url);
+
+        //action
+        $action = (isset($url[0]) && $url[0] != '') ? $url[0] . 'Action': 'indexAction';
+        $action_name = $controller;
+        array_shift($url);
+
+        //params
+        $queryParams = $url;
+
+        $dispatch =  new $controller($controller_name, $action);
+
+        if(method_exists($controller, $action)) {
+            call_user_func_array([$dispatch, $action], $queryParams);
+        } else {
+            die('Method does not exist in the controller \"' . $controller_name . '\"');
+        }
+
+       
         
     }
 }
